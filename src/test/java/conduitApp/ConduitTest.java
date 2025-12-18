4b1cd93bb02667dc436afb99c62bd9e17941557f
Naveen <naveennagarajan34@gmail.com>
2025-12-18 16:01:44 +0530

    cucumber reports implemented

--- src/test/java/conduitApp/ConduitTest.java
+++ src/test/java/conduitApp/ConduitTest.java
@@ -2,21 +2,34 @@ package conduitApp;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-import com.intuit.karate.junit5.Karate;
+
+import net.masterthought.cucumber.ReportBuilder;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import java.io.File;
+
+import net.masterthought.cucumber.Configuration;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
+
 
 class ConduitTest {
 
     @Test
     void testParallel() {
     Results results = Runner.path("classpath:conduitApp")
-    // .outputCucumberJson(true)
+    .outputCucumberJson(true)
     .parallel(2);
     // @parallel=false  can be used on the top of the feature or Scenario if needed to run sequentially 
+        
+    generateReport(results.getReportDir());
+    assertTrue(results.getFailCount() == 0, results.getErrorMessages());
 
-    assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
 
     // @Karate.Test
@@ -28,4 +41,13 @@ class ConduitTest {
     // Karate runAll() {
     //     return Karate.run().relativeTo(getClass());
     // }
+
+        public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "conduitApp");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
+    }
 }
